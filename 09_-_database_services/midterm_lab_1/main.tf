5d6b83cb8eea3bba1649f6e0c82321ce409bdb88
Chris King-Parra <chris@kingparra.work>
2022-11-06 13:46:15 -0500

    Add work so far

--- 09_-_database_services/midterm_lab_1/main.tf
+++ 09_-_database_services/midterm_lab_1/main.tf
@@ -4,13 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    # To generate the ssh kepair
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.3"
     }
+    # To download the website site content
     http = {
       source = "hashicorp/http"
       version = "3.2.0"
+    }
+    # To name the bucket
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
     }
   }
 }
@@ -21,121 +28,119 @@ provider "aws" {
   shared_credentials_files = var.account_details.shared_credentials_files
   default_tags {
     tags = {
-      Name = "midterm_project_1"
+      Lab = "midterm_project_1"
       Session = "1"
     }
   }
 }
 
 # Prerequistes
-#################################################################################################
-resource "aws_s3_bucket" "webapp_bucket" {
-  bucket = "webapp-bucket-ergo-proxy-relevent-beaver"
+resource "random_pet" "webapp_bucket_petname" {
+  keepers = {
+    # Generate a new pet name each time we switch to a vpc id
+    bucket_id = aws_vpc.vpc.id
+  }
 }
 
-resource "aws_s3_object" "sample_page_object" {
+resource "aws_s3_bucket" "webapp_bucket" {
+  bucket = random_pet.webapp_bucket_petname.id
+}
+
+resource "aws_s3_object" "webapp_bucket_object" {
   bucket = aws_s3_bucket.webapp_bucket.id
   key = "samplepage.php"
   content = data.http.sample_page_contents.response_body
 }
 
-/*
 resource "aws_iam_role" "webapp_role" {
-  # policy AmazonS3FullAccess
   name = "midterm-ec2-s3-role"
+  # In order to assume the role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "managed_policy" {
+  role = aws_iam_role.webapp_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_eip" "eip" {
-  # region = "us_east_1" # north virginia
+  # This will be associated with the nat gateway
 }
-*/
+
 
 # Create a vpc with private and public subnets
-#################################################################################################
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-  # region = "us_east_1" # north virginia
-  # name = "midterm"
-  # Name tag auto-generation: midterm
-  # number of AZs: 2
-  # AZs: us-east-1a, us-east-1b
-  # number of public subnets: 2
-  # number of private subnets: 2
-  #  Public subnet CIDR block in us-east-1a: 10.0.0.0/24
-  #  Public subnet CIDR block in us-east-1b 10.0.1.0/245.
-  #  Private subnet CIDR block in us-east-1a: 10.0.2.0/24
-  #  Private subnet CIDR block in us-east-1b 10.0.3.0/24
-  # NGW: In 1 AZ
-  # VPC endpoints: None
-  # Enable DNS hostnames: yes
-  # Enable DNS resolution: yes
-}
-
-/*
-resource "aws_internet_gateway" "igw" {
-}
+# see networking.tf
 
 
 # Create a vpc security group for a public web server
-#################################################################################################
-resource "aws_security_group" "public_subnet_sg" {
-  name        = "web"
-  description = "ELB for a webserver cluster"
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "webserver_sg" {
+    name        = "tutorial-securitygroup"
+    description = "Tutorial Security Group"
+    vpc_id      = aws_vpc.vpc.id
+    ingress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
 }
-
-resource "aws_subnet" "public_subnet" {
-}
-
 
 # Create a vpc security group for a private DB instance
-#################################################################################################
-resource "aws_security_group" "private_subnet_sg" {
-  name        = "web"
-  description = "ELB for a webserver cluster"
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "db-sg" {
+    name        = "tutorial-db-securitygroup"
+    description = "Tutorial DB Instance Security Group"
+    vpc_id      = aws_vpc.vpc.id
+    ingress {
+        from_port       = 3306
+        to_port         = 3306
+        protocol        = "tcp"
+        security_groups = [aws_security_group.webserver_sg.id]
+        self            = false
+    }
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
 }
-
-resource "aws_subnet" "private_subnet" {
-}
-
 
 # Create a DB subnet group
-#################################################################################################
 resource "aws_db_subnet_group" "dsg" {
   name = "tutorial-db-subnet-group"
   description = "Tutorial DB Subnet Group"
   # vpc: "midterm-vpc" (vpc-identifier)
   # availability_zones = ["us-east-1a", "us-east-1b"]
-  # subnets = [aws_subnet.public_subnet.public_subnet_sg.id, aws_subnet.private_subnet.private_subnet_sg.id]
-
+  subnet_ids = [ aws_subnet.subnet-023f4bbe3d8e5dac8-midterm-subnet-private2-us-east-1b.id, aws_subnet.subnet-00e517c733f3793c7-midterm-subnet-private1-us-east-1a.id]
 }
 
 
 # Now lets create a DB instance
-#################################################################################################
+##################################################################################
 # resource: rds database
 # ^^^^^^^^^^^^^^^^^^^^^^
 # engine type: mysql
@@ -171,10 +176,8 @@ resource "aws_db_subnet_group" "dsg" {
 
 
 # Now create an ec2 instance and install a web server
-#################################################################################################
+##################################################################################
 
 
 # Connect your apache web server to your db instance
-#################################################################################################
-
-*/
+##################################################################################
