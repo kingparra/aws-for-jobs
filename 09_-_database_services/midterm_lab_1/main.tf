7e357a26e5a0112df4972f606ce8d6dae8d6e05f
Chris King-Parra <chris@kingparra.work>
2022-11-06 14:21:15 -0500

    Revert "Rename public and private subnet resources"
    This reverts commit 249fe83c5f58c7dc635d2923f61143e11d9eeb72.

--- 09_-_database_services/midterm_lab_1/main.tf
+++ 09_-_database_services/midterm_lab_1/main.tf
@@ -133,7 +133,9 @@ resource "aws_security_group" "db-sg" {
 resource "aws_db_subnet_group" "dsg" {
   name = "tutorial-db-subnet-group"
   description = "Tutorial DB Subnet Group"
-  subnet_ids = [ aws_subnet.subnet-private2.id, aws_subnet.midterm-subnet-private1.id]
+  # vpc: "midterm-vpc" (vpc-identifier)
+  # availability_zones = ["us-east-1a", "us-east-1b"]
+  subnet_ids = [ aws_subnet.subnet-023f4bbe3d8e5dac8-midterm-subnet-private2-us-east-1b.id, aws_subnet.subnet-00e517c733f3793c7-midterm-subnet-private1-us-east-1a.id]
 }
 
 
