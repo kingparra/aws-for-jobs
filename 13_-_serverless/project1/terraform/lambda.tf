9dc8db1ed1c2147f655268780ab9a5a0311ff2b6
Chris King-Parra <chris@kingparra.work>
2023-06-29 16:32:44 -0400

    Add lambda function

---
+++ 13_-_serverless/project1/terraform/lambda.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type = "zip"
+  source_file = "${path.module}/assets/function/handler.py"
+  output_path = "${path.module}/assets/function/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "yt-website-lambda"
+  runtime = "python3.9"
+  handler = "handler.lambda_handler"
+  filename = "${path.module}/assets/function/lambda_function_payload.zip"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  role = aws_iam_role.iam_for_lambda.arn
+}
+
+/*
+resource "aws_apigatewayv2_api" "lambda_gw" {
+  name = "example-http-api"
+  protocol_type = "HTTP"
+  # cors_configuration = ... https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html
+}
+*/
