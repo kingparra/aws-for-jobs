38acda5bc255ceee73236183aae6298f35d5012c
Chris King-Parra <chris@kingparra.work>
2023-08-24 13:26:33 -0400

    Update stub program for deploy-hs

--- 03_-_ec2_basics/deploy-an-instance/deploy-hs/app/Main.hs
+++ 03_-_ec2_basics/deploy-an-instance/deploy-hs/app/Main.hs
@@ -1,4 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+module Main where
+import Amazonka
+import Amazonka.EC2
+import Control.Lens ((^.), (?~))
+import Data.Text hiding (map)
 
+
+data Options = Options {
+      ami_id :: Text
+    , vpc_id :: Text
+} deriving Show
+
+
+main :: IO ()
 main = do
-  putStrLn "TODO Implement EC2 deployment using Amazonka"
+  putStrLn "Stub for program that launches an EC2 instance"
