eee7020a8f2ad37eb616757ea1261429448cb13b
Chris King-Parra <chris@kingparra.work>
2023-09-10 17:02:27 -0400

    Add security group for boto3 instance

--- 03_-_ec2_basics/deploy-an-instance/python-boto3/src/python_boto3/__main__.py
+++ 03_-_ec2_basics/deploy-an-instance/python-boto3/src/python_boto3/__main__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import boto3
-import time
-ec2client = boto3.client('ec2')
-ec2 = boto3.resource('ec2')
 
+ec2client = boto3.client('ec2') # returns json
+ec2 = boto3.resource('ec2') # returns objects
+
+# Generatea list of amazon linux 2 ami ids
 img_response = ec2client.describe_images(
     Filters=[
         {'Name': 'name', 'Values': ['amzn2-ami-hvm-*']},
@@ -14,10 +15,32 @@ img_response = ec2client.describe_images(
     Owners=['amazon']
 )
 
+# Get the most recent ami from the list
 latest_ami_id = sorted(img_response['Images'],
                        key=lambda x: x['CreationDate'],
                        reverse=True)[0]['ImageId']
 
+# Create a security group that allows ssh ingress
+sg = ec2.create_security_group(
+  GroupName='boto3-instance-sg',
+  Description='SG for instance created with boto3'
+)
+
+sg.authorize_ingress(
+    IpPermissions=[
+        {
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        }
+    ]
+)
+
+# Create an instance with the latest amazon linux 2 ami,
+# our security group, and the keypair 'precision'.
+# Use 't2.micro' as the instance type.
+# Name it 'boto3-instance'.
 instance = ec2.create_instances(
     ImageId=latest_ami_id,
     InstanceType='t2.micro',
@@ -28,17 +51,19 @@ instance = ec2.create_instances(
             'DeviceIndex': 0,
         },
     ],
+    SecurityGroupIds=[sg.id],
+    TagSpecifications=[{
+      'ResourceType': 'instance',
+      'Tags': [{'Key': 'Name', 'Value': 'boto3-instance'}]}],
     Monitoring={'Enabled': True},
     MinCount=1,
     MaxCount=1
     )[0]
 
-# if the instance is up and has status OK, after 40 attempts
-#   print the instance id and public ip address of that instance
-
 try:
+  # Wait until the instance is up
   instance.wait_until_running()
-   # needed to refresh ip info https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/instance/index.html#EC2.Instance
+  # Refresh the ip info
   instance.reload()
   print(f"{instance.instance_id} is up and running on {instance.public_ip_address}")
 except e:
